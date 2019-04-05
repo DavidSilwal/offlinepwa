5bfd19b9ec4e7f55c75c3c9142d15db6359bbc1c
Justin Schwartzenberger <schwarty@gmail.com>
2019-04-05 16:19:30 -0700

    add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    
+    <h1>Session Tracker</h1>
+    <p>Log a new session</p>
+    <form #f="ngForm" (ngSubmit)="save(f)">
+      Start: <input type="text" name="startTime" ngModel>
+      End: <input type="text" name="endTime" ngModel>
+      <button type="submit">Save</button>
+    </form>
   `,
   styles: []
 })
 export class AppComponent {
-  
+  save(form: FormGroup): void {
+    // save to local storage
+    console.log(form.value);
+    // clear the form
+    form.reset();
+  }
 }
