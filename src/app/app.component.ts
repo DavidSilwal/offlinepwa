f7ec021dcf881f741a948cb1963ad9126673f222
Justin Schwartzenberger <schwarty@gmail.com>
2019-04-05 17:42:56 -0700

    add local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { LocalStorage } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,44 @@ import { FormGroup } from '@angular/forms';
       End: <input type="text" name="endTime" ngModel>
       <button type="submit">Save</button>
     </form>
+    <p>Past sessions</p>
+    <ul>
+      <li *ngFor="let session of sessions; let i=index">
+        {{ session.startTime }} to {{ session.endTime }}
+        <button (click)="delete(i)">Delete</button>
+      </li>
+    </ul>
   `,
   styles: []
 })
-export class AppComponent {
-  save(form: FormGroup): void {
-    // save to local storage
-    console.log(form.value);
-    // clear the form
-    form.reset();
+export class AppComponent implements OnInit {
+  sessions: Session[];
+  constructor(private localStorage: LocalStorage) {}
+
+  ngOnInit(): void {
+    this.localStorage.getItem<Session[]>('sessions')
+      .subscribe((sessions) => {
+        this.sessions = (sessions === null) ? [] : sessions;
+      });
   }
+
+  save(form: FormGroup): void {
+    console.log(form.value);
+    this.sessions.push(form.value);
+    this.localStorage.setItem('sessions', this.sessions)
+      .subscribe(() => {
+        form.reset();
+      });
+  }
+
+  delete(index: number): void {
+    this.sessions.splice(index, 1);
+    this.localStorage.setItem('sessions', this.sessions)
+      .subscribe();
+  }
+}
+
+interface Session {
+  startTime: string;
+  endTime: string;
 }
